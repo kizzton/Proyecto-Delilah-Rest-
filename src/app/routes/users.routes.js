8e5c45391bd8aef886b7e2279f208b7ba1777717
kizzton <kizzton@gmail.com>
2021-03-03 18:18:42 +0100

    Create users.routes.js

---
+++ src/app/routes/users.routes.js
@@ -0,0 +1,17 @@
+const { Router } = require('express')
+const UsersController = require('../controllers/user.controller')
+const { verifyToken, verifyRole } = require('../middlewares/auth.middleware')
+const router = Router()
+
+router
+    .get('/', verifyToken, verifyRole, UsersController.getAll)
+
+    .post('/signin', UsersController.add)
+
+    .get('/:id', verifyToken, UsersController.getById)
+
+    .put('/edit/:id', verifyToken, verifyRole, UsersController.updateRegistry)
+    
+    .delete('/delete/:id', verifyToken, verifyRole, UsersController.deleteById)
+
+module.exports = router
