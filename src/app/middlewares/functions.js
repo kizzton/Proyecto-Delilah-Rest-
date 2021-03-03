640639cc34507a51a3af0c8de83584d3b1adf7b9
kizzton <kizzton@gmail.com>
2021-03-03 18:14:25 +0100

    Create functions.js

---
+++ src/app/middlewares/functions.js
@@ -0,0 +1,9 @@
+const filterParams = (inputObject) => {
+    
+    for (let params in inputObject){
+        !inputObject[params] && delete inputObject[params]
+        return inputObject 
+    }
+}
+
+module.exports = { filterParams }
